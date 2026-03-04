7e66f9c9eac4361603541635a3072920eb64e79a
Karen <Karen.khudaverdyan@gmail.com>
2026-03-05 01:11:33 +0400

    Clean up code

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from handle_data import save_to_csv
 
 urls = {
     'Hacker News' : 'https://brutalist.report/source/hn?hours=6', # Hacker News
@@ -10,25 +11,37 @@ urls = {
 }
 
 def fetch_headlines():
-
+    """
+    Fetches headlines from different sources on brutalist.report
+    """
     headlines = []
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/122.0.0.0'}
 
     for name, url in urls.items():
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        try:
+            page = requests.get(url, headers=headers, timeout=10)
+            soup = BeautifulSoup(page.text, 'html.parser')
 
-        li_tags = soup.find_all('li')
+            li_tags = soup.find_all('li')
 
-        for tag in li_tags:
-            try:
-                entry = {
-                    'headline' : tag.find('a').get_text(strip=True),
-                    'source' : name
-                }
-                headlines.append(entry)
-            except AttributeError as e:
-                pass
+            for tag in li_tags:
+                try:
+                    a_tag = tag.find('a')
+                    entry = {
+                        'headline': a_tag.get_text(strip=True),
+                        'source': name,
+                        'link': a_tag['href']
+                    }
+                    headlines.append(entry)
+                except AttributeError as e:
+                    pass
+        except Exception as e:
+            print(f"Error {e} while fetching the headlines")
 
     return headlines
 
-print(fetch_headlines())
+if __name__ == "__main__":
+    data = fetch_headlines()
+    if data:
+        save_to_csv(data)
+        print(f"Saved {len(data)} headlines.")
